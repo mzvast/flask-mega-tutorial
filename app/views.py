a8ce00d28effeafa2b14410f31ab287549315971
mzvast <mzvast@gmail.com>
2015-10-10 07:22:21 +0000

    templates added

--- app/views.py
+++ app/views.py
@@ -1,6 +1,25 @@
+from flask import render_template
 from app import app #imports the app variable from our app package 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Miguel'}  # fake user
+    posts = [  # fake array of posts
+        { 
+            'author': {'nickname': 'John'}, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': {'nickname': 'Susan'}, 
+            'body': 'The Avengers movie was so cool!' 
+        },
+        {
+            'author':{'nickname':'Zhao,the sky fucker'},
+            'body':'I am not Fu!'
+        }
+    ]
+    return render_template("index.html",
+                           title='Home',
+                           user=user,
+                           posts=posts)
