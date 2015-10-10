d50ae4e692f9cc2dbaf4ed6bb4b9e91bc6879ff7
mzvast <mzvast@gmail.com>
2015-10-10 14:44:08 +0000

    login ready

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 #!flask/bin/python
 from app import app
 import os
-app.run(host = os.getenv('IP','0.0.0.0'), port = int(os.getenv('PORT',8080)),debug=True)
+host = os.getenv('IP','0.0.0.0')
+port = int(os.getenv('PORT',8080))
+app.run(host, port,debug=True)
